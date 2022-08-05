49d915874102250d096df9251904a6390f0666f6
Marcos Esteban <83374363+MarcosGodoydevelopped@users.noreply.github.com>
2022-08-05 14:53:28 -0300

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,14 +6,14 @@ function Navbar(){
     <div className="bg-neutral-800 flex justify-between px-20 py-4">
     
     <Link className="text-white font-bold" to="/"> 
-      <h1>React MySql</h1>
+      <h1>Listado De Tareas</h1>
     </Link>
     <ul className="flex gap-x-1">
         <li>
             <Link to="/" className="bg-slate-200 px-2 py-1">Home</Link>
         </li>
         <li>
-            <Link to="/new" className="bg-teal-200 px-2 py-1">Create Tasks</Link>
+            <Link to="/new" className="bg-teal-200 px-2 py-1">Crear Tarea</Link>
         </li>
     </ul>
     </div>
